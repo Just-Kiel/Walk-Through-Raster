f1f1e77c0bc40f341f5a0ced1bc8e19382c9d26d
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2022-03-27 20:02:02 +0200

    Buttons Corrected and visual too

--- sketch.js
+++ sketch.js
@@ -1,49 +1,27 @@
-let TEST=[];
+let shape=[];
 
 let buttons=[];
 
 let colorPik;
+let currentColor = 'rgba(255, 165, 0, 0.5)';
 
-let slider, sliderText;
+let slider, sliderText, sliderValue;
 
 let textOverlap;
 
 var activatedOverlap = false;
 
 // Fonctions de stylisation des boutons
-function buttonOut(){
-  this.style('background-color','#242424');
-  this.style('color','#FFFFFF');
+function buttonOver(target, c, b) {
+  target.style('background-color',b);
+  target.style('color',c);
 }
-
-function greenButtonOver(){
-  this.style('background-color','#9ACD32');
-  this.style('color','#000000');
-}
-
-function pinkButtonOver(){
-  this.style('background-color','#FF69B4');
-  this.style('color','#000000');
-}
-
-function blueButtonOver(){
-  this.style('background-color','#87CEEB');
-  this.style('color','#000000');
-}
-
-function orangeButtonOver(){
-  this.style('background-color','#FFA500');
-  this.style('color','#000000');
-}
-
-function violetButtonOver(){
-  this.style('background-color','#8A2BE2');
-  this.style('color','#000000');
-}
-
 
 function setup() {
-  textFont('Montserrat');
+  
+  //////////HTML/////////////
+  
+  
   let title = createElement('h1', 'Walk Through Raster');
   title.style('color', '#FFA500');
   let subtitle=createElement('h2', 'by Frieder Nake');
@@ -51,58 +29,82 @@ function setup() {
   let presentation = createElement('p', '<strong>Made in :</strong> 1967 <br> <strong>Dimensions :</strong> 25.75 inches per 25.75 inches (65,4 m x 65,4cm)(with the frame) <br> 50,8 x 50,8 cm (without frame) <br><strong>Machine used :</strong> ZUSE Graphomat Z64 draw the image <br><strong>Computer used :</strong> Telefunken TR4 <br><strong>Software :</strong> Walk-through-Raster <br><strong> Programming language : </strong>ALGOL 60');
   
   let ourInterpretation = createElement('h3', "Our interpretation :");
-  ourInterpretation.style('color', '#FFA500	');
+  ourInterpretation.style('color', '#FFA500');
   
-  let interpretation = createElement('p', 'blabladela interpretation cest koul');
-  
-   let colorInterpretation = createElement('h4', "Change Color Mode");
-  colorInterpretation.style('color', '#87CEEB	');
-  
-  let colorExplanation = createElement('p', 'pourquoi les couleurs cest BO');
-  
-let artworkInterpretation = createElement('h4', "Change Artwork Mode");
-  artworkInterpretation.style('color', '#87CEEB	');
-  
-  let artworkExplanation = createElement('p', 'pourquoi plusieurs choix + overlap ');
-  
-  let otherInterpretation = createElement('h4', "Other modifications");
-  otherInterpretation.style('color', '#87CEEB');
-  
-  let otherExplanation = createElement('p', 'tout le reste, les modifs etc ');
-  
+  let interpretation = createElement('p', 'To extend this work, we decided to do an interactive version. Several buttons can change the algorithm and a presentation of the work makes the whole more user friendly. Changes of colors, quantities and regeneration can be found. Buttons were also designed to try different logics of understanding the artwork. We also added a button to download our P5.js code or the canvas in a PNG.');
   
   let names=createElement('h5',"Aurore Lafaurie & Sarah N'GOTTA");
   names.style('color','#242424');
+  
+  let firstStep=createElement('p',"First of all, please select all the artwork's settings you want.");
+  firstStep.style('color','#242424');
+  firstStep.position(810, 0);
+  
+  
+  let lastStep=createElement('p',"If you want to regenarate a new artwork with the current parameters, please click on the following button.");
+  lastStep.style('color','#242424');
+  lastStep.position(810, 20+3*80);
+
+  
+    let finalStep=createElement('p',"Congratulations ! You can save the artwork and the source code ! <br><br>If you want to learn more about this artwork, scroll down !");
+  finalStep.style('color','#242424');
+  finalStep.position(810, 50+5.5*80);
+
+  
   
   createCanvas(800, 800);
   background('#fefaef');
   
   //Initialisation du slider de quantité
   slider = createSlider(0, 400, 100, 5);
-  slider.position(810, 15);
+  slider.position(810, 60);
   slider.style('width', '80px');
   sliderText = createP();
   sliderText.style('color', '#242424');
-  sliderText.position(900, 0);
+  sliderText.position(900, 45);
   sliderText.html('squares quantity');
+  sliderValue = slider.value();
+  
+  let fontmontserrat = [presentation, interpretation, firstStep, lastStep, finalStep];
+  
+  let fontbebas = [title, subtitle, ourInterpretation, names];
+  
+  for(let i=0; i<5; i++){
+    fontmontserrat[i].style('font-family', 'Montserrat');
+  }
+  
+  for(let i=0; i<4; i++){
+    fontbebas[i].style('font-family', 'Bebas Neue');
+  }
   
   //Initialisation des boutons
-  buttons[0] = createButton('save artwork');
-  buttons[0].mouseOver(greenButtonOver);
+  buttons[3] = createButton('save artwork');
+  buttons[3].mouseOver(function() {
+    buttonOver(this, "#000000", "#9ACD32");
+  });
 
-  buttons[1] = createButton('new artwork');
-  buttons[1].mouseOver(pinkButtonOver);
+  buttons[2] = createButton('regenerate artwork');
+  buttons[2].mouseOver(function() {
+    buttonOver(this, "#FFFFFF", "#242424");
+  });
   
-  buttons[2] = createButton('change color');
-  buttons[2].mouseOver(blueButtonOver);
+  buttons[0] = createButton('change color');
+  buttons[0].mouseOver(function() {
+    buttonOver(this, "#000000", "#87CEEB");
+  });
   
   textOverlap = 'turn on overlap function';
-  buttons[3] = createButton(textOverlap);
-  buttons[3].mouseOver(orangeButtonOver);
+  buttons[1] = createButton(textOverlap);
+  buttons[1].mouseOver(function() {
+    buttonOver(this, "#000000", "#FFA500");
+  });
   
   
-  buttons[4]=createElement('button').html("<a style='text-decoration:none;color:#ffffff' href='./sketch.js' download='NGOTTA_LAFAURIE_p5js.js'>source code</a>");
-  buttons[4].mouseOver(violetButtonOver);
+  buttons[4]=createElement('button').html("<a style='text-decoration:none;color:#ffffff' href='./sketch.js' download='NGOTTA_LAFAURIE_p5js.js'>download source code</a>");
+  buttons[4].mouseOver(function() {
+    buttonOver(this, "#000000", "#8A2BE2");
+  });
+  
   
   
   for(let i=0; i<5; i++){
@@ -112,29 +114,49 @@ let artworkInterpretation = createElement('h4', "Change Artwork Mode");
     buttons[i].style('color','#ffffff'); 
     buttons[i].style('border','none');
     buttons[i].style('background-color','#242424');
-    buttons[i].position(810, 50+i*80);  
-    buttons[i].mouseOut(buttonOut);
+    buttons[i].position(810,100+i*110); 
+    buttons[i].style('font-family', 'Montserrat');
+    buttons[i].mouseOut(function() {
+    buttonOver(this, "#FFFFFF", "#242424");
+    });
   }
-    buttons[0].mousePressed(saveCanva);
-    buttons[1].mousePressed(newVersion);
-    buttons[2].mousePressed(changeColorButton);
-    buttons[3].mousePressed(versionWithOverlap);
+    buttons[3].mousePressed(saveCanva);
+    buttons[2].mousePressed(newVersion);
+  
+    buttons[2].mouseOut(function() {
+    buttonOver(this, "#000000", "#FF69B4");
+    });
+    buttons[2].style('color',"#000000"); 
+    buttons[2].style('background-color',"#FF69B4");
+  
+    buttons[4].position(950, 100+3*110);
+    buttons[0].mousePressed(changeColorButton);
+    buttons[1].mousePressed(versionWithOverlap);
   
   
   // Création de la première oeuvre
   newVersion();
 }
 
+function draw(){
+  if(slider.value()!=sliderValue){
+    newVersion();
+    sliderValue = slider.value();
+  }
+}
+
 
 function versionWithOverlap(){
   if(!activatedOverlap){
     textOverlap = 'turn off overlap function';
-    buttons[3].html(textOverlap);
+    buttons[1].html(textOverlap);
     activatedOverlap = true;
+    newVersion();
   } else {
     textOverlap = 'turn on overlap function';
-    buttons[3].html(textOverlap);
+    buttons[1].html(textOverlap);
     activatedOverlap = false;
+    newVersion();
   }
 }
 
@@ -143,18 +165,22 @@ function changeColorButton(){
   let colorstopik=['rgba(154,205,50,0.5)', 'rgba(255, 105, 180,0.5)', 'rgba(135, 206, 250,0.5)', 'rgba(255, 0, 0,0.5)', 'rgba(148, 0, 211,0.5)','rgba(255, 165, 0, 0.5)'];
   
   colorPik=random(colorstopik);
+  while(currentColor == colorPik){
+    colorPik=random(colorstopik);
+  }
+  currentColor = colorPik;
   
 background('#fefaef');
   // Changement de la couleur pour les rectangles non noirs et dessins de tous
   for(let i=0; i<slider.value(); i++){
-    if(TEST[i]==undefined){
-      TEST[i] = new shape_rectangle();
+    if(shape[i]==undefined){
+      shape[i] = new shape_rectangle();
     }
-      if(TEST[i].col != 'rgba(0,0,0,1)'){
-        TEST[i].changeCol(colorPik);
-        TEST[i].render();
+      if(shape[i].col != 'rgba(0,0,0,1)'){
+        shape[i].changeCol(colorPik);
+        shape[i].render();
       } else {
-        TEST[i].blackRender();
+        shape[i].blackRender();
       }
 }
 }
@@ -170,21 +196,22 @@ function newVersion(){
   if(activatedOverlap){
     print('avec overlap');
     for(let i=0; i<slider.value(); i++){
-	TEST[i] = new shape_rectangle();
+	shape[i] = new shape_rectangle();
     let count = 0;
       for(let j=0; j<i; j++){
-        let o = overlap(TEST[j], TEST[i]);
+        let o = overlap(shape[j], shape[i]);
         if(i!=j && !o){
           count+=1;
         }   
       }   
       if(count<=40){
-        TEST[i].render();
+        shape[i].changeCol(currentColor);
+        shape[i].render();
       }
       //changement en noir si >40 couches 
     else {
-        TEST[i].changeCol('rgba(0,0,0,1)');
-        TEST[i].blackRender();
+        shape[i].changeCol('rgba(0,0,0,1)');
+        shape[i].blackRender();
     } 
     }
   } else {
@@ -192,13 +219,14 @@ function newVersion(){
   
   let littleCount = slider.value()/4;
   for(let i=0; i<littleCount*3; i++){
-	TEST[i] = new shape_rectangle();  
-    TEST[i].render();
+	shape[i] = new shape_rectangle(); 
+    shape[i].changeCol(currentColor);
+    shape[i].render();
   }
   for(let j=0; j<littleCount; j++){
-    TEST[j] = new shape_rectangle();
-    TEST[j].changeCol('rgba(0,0,0,1)');
-    TEST[j].blackRender();
+    shape[j] = new shape_rectangle();
+    shape[j].changeCol('rgba(0,0,0,1)');
+    shape[j].blackRender();
   }
   }
 }
@@ -305,7 +333,6 @@ function overlap(rect2, rect1) {
     infRightAngleOld=rect2.x+rect2.width+rect2.height;
   } else {
     print('black comparing');
-    //à vérifier si ça marche
     supLeftAngleOld=rect2.x
     supRightAngleOld=rect2.x+rect2.height;
     infLeftAngleOld=rect2.x+rect2.width;
